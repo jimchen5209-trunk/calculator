2ceb04bbe85d9e9b1d5448ed26934634d06aec67
jimchen5209 <jimchen5209@gmail.com>
2022-07-13 17:06:27 +0800

    HW20220713

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="number" v-model="a">
+  <select name="operand" v-model="operand">
+    <option value="" disabled selected>選擇運算子</option>
+    <option v-for="symbol in operandSymbles" :key="symbol" :value="symbol">{{ symbol }}</option>
+  </select>
+  <input type="number" v-model="b">
+  = {{ result }}
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script setup>
+import { ref, computed, reactive } from 'vue'
+const operandSymbles = reactive(['+','-','*','/']);
+const a = ref('')
+const b = ref('')
+const operand = ref('')
+const result = computed(() => { 
+  switch (operand.value){
+    case '+':
+      return a.value + b.value
+    case '-':
+      return a.value - b.value
+    case '*':
+      return a.value * b.value
+    case '/':
+      return a.value / b.value
+    default:
+      return ''
   }
-}
+ })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
